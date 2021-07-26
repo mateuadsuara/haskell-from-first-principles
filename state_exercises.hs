80816d1b4f1d2c1fbee082dff8b13d1b758078ea
Mateu Adsuara <mateuadsuara@gmail.com>
2021-07-26 20:02:33 +0200

    Continue Moi refactor

--- state_exercises.hs
+++ state_exercises.hs
@@ -4,6 +4,8 @@
 
 import System.Random (StdGen, randomR, mkStdGen)
 import Data.Monoid
+import Control.Arrow
+import Debug.Trace
 
 rollsToGetN :: Int -> StdGen -> Int
 rollsToGetN n = go 0 0
@@ -69,7 +71,7 @@ instance Applicative (Moi s) where
 
 instance Monad (Moi s) where
   return = pure
-  
+
   (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
   (Moi fs) >>= ga = Moi $ \s -> let (a, s') = fs s
                                     Moi bs = ga a
@@ -78,9 +80,21 @@ instance Monad (Moi s) where
 get :: Moi s s
 get = Moi $ \s -> (s, s)
 
+gets :: (s -> a) -> Moi s a
+--gets f = Moi $ \s -> (f s, s)
+--gets f = do
+--  s <- get
+--  return $ f s
+--gets f = get >>= return . f
+gets f = f <$> get
+
 put :: s -> Moi s ()
 put s = Moi $ const ((), s)
 -- put = Moi . const . ((),)
+
+modify :: (s -> s) -> Moi s ()
+--modify f = Moi $ \s -> ((), f s)
+modify f = get >>= put . f
 
 roll :: Moi StdGen Int
 roll = do
@@ -105,9 +119,20 @@ rollsCountLoggedMoi n = fst . runMoi(go 0 mempty)
 --repl> sum (map (\(Die n) -> n) xs)
 --repl> sum xs -- Tras derivar `Num` en `Die`
 
-type RollM = Moi (StdGen, Result) 
+type RollM = Moi (StdGen, Result)
 
-roll' :: RollM Int 
+getGen :: RollM StdGen
+getGen = gets fst
+
+putGen :: StdGen -> RollM ()
+--putGen gen = modify $ \(_, r) -> (gen, r)
+putGen = modify . first . const
+
+modifyResult :: (Result -> Result) -> RollM ()
+--modifyResult f = modify $ \(g, r) -> (g, f r)
+modifyResult = modify . second
+
+roll' :: RollM Int
 roll' = do
   (gen, result) <- get
   let (dieInt, nextGen) = randomR (1, 6) gen
@@ -122,7 +147,7 @@ storeDie n = do
 
 rollsCountLoggedMoi' :: Int -> StdGen -> Result
 rollsCountLoggedMoi' n gen = let state = execMoi (go 0) (gen, mempty)
-                                 (gen, result) = state
+                                 (_, result) = state
                               in result
   where
     go :: Int -> RollM ()
@@ -133,7 +158,47 @@ rollsCountLoggedMoi' n gen = let state = execMoi (go 0) (gen, mempty)
           storeDie dieInt
           go (sum + dieInt)
 
+--repl> rollsCountLoggedMoi' 40 (mkStdGen 1233)
+--repl> (Sum {getSum = 13},[Die 6,Die 4,Die 2,Die 4,Die 5,Die 1,Die 5,Die 4,Die 1,Die 2,Die 1,Die 2,Die 6])
+
+rollsCountLoggedMoi'' :: Int -> StdGen -> Result
+rollsCountLoggedMoi'' n gen = let state = execMoi (go 0) (gen, mempty)
+                                  (_, result) = state
+                              in result
+  where
+    go :: Int -> RollM ()
+    go sum
+      | sum >= n  = return ()
+      | otherwise = do
+          dieInt <- roll''
+          storeDie'' dieInt
+          go (sum + dieInt)
+
+--repl> rollsCountLoggedMoi'' 40 (mkStdGen 1233)
+--repl> (Sum {getSum = 13},[Die 6,Die 4,Die 2,Die 4,Die 5,Die 1,Die 5,Die 4,Die 1,Die 2,Die 1,Die 2,Die 6])
+
+roll'' :: RollM Int
+roll'' = do
+  (dieInt, nextGen) <- randomR (1, 6) <$> getGen
+  putGen nextGen
+  return dieInt
+
+storeDie'' :: Int -> RollM ()
+storeDie'' n = modifyResult (<> (Sum 1, [Die n]))
+
+--repl> rollsCountLoggedMoi'' 40 (mkStdGen 1233)
+
 -- TODO:
 -- modify
 -- getGen/getResult
 -- putGen/putResult
+--
+--
+-- *Main Control.Arrow> :t (&&&)
+-- (&&&) :: Arrow a => a b c -> a b c' -> a b (c, c')
+-- *Main Control.Arrow> (*2) &&& (+7) $ 6
+-- (12,13)
+-- *Main Control.Arrow> :t (***)
+-- (***) :: Arrow a => a b c -> a b' c' -> a (b, b') (c, c')
+-- *Main Control.Arrow> (*2) *** (+7) $ (6, 9)
+-- (12,16)
